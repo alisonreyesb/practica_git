941510fe95134d1bf007cd10a022b9f07bfcab7c
Alison Kamila Reyes Baquero <alisonreyesuwu.com>
2025-11-13 08:30:39 -0500

    feat(practica_git_python): Se realizan correciones propuestas por santiago avila, y se corrige las funciones, el area del triangulo se agrga /2,ya que faltaba

--- calculos.py
+++ calculos.py
@@ -4,4 +4,5 @@ def area_cuadrado(lado):
 
 #Funcion designada para calcular el area del triangulo
 def area_triangulo(base, altura):
-    return base * altura
+    return base * altura/2
+#se observa la funcion incopleta ya que para calcular el area del triangulo se debe dividir entre 2
