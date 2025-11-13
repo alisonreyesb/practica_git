ec1ee05c6a17b9617ab98c549570843ff42b144d
Alison Kamila Reyes Baquero <alisonreyesuwu.com>
2025-11-13 09:40:36 -0500

    feat:Se hace la creacion de un menu con 4 opciones, la primera le pide las longitudes al usuario y se calcula el area del cuadrado, la segunda calcula el area del triangulo, la tercera suma dos numeros y la cuarta sale del programa, llamando las funciones del los archivos calculos y functions, el menu se ejecuta en el documento main

--- calculos.py
+++ calculos.py
@@ -5,4 +5,4 @@ def area_cuadrado(lado):
 #Funcion designada para calcular el area del triangulo
 def area_triangulo(base, altura):
     return base * altura/2
-#se observa la funcion incopleta ya que para calcular el area del triangulo se debe dividir entre 2
+#se observa la funcion incompleta ya que para calcular el area del triangulo se debe dividir entre 2
--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 #importacion de archivos
-from fuctions import suma
+from functions import suma
+from calculos import area_triangulo
+from calculos import area_cuadrado
 #programa
-resultados=suma(5,10)
+print("Practicas git")
 
-print(resultados)
+while True:
+    #Creacion del menu y solicitud de operacion
+    print("Seleccione la operacion a realizar:")
+    print("1.Area del cuadrado")
+    print("2.Area del triangulo")
+    print("3.Suma")
+    print("4.Salir")
+    #Se solicita la opcion al usuario
+    eleccion = input("Ingrese la opcion: ")
+    #si el ususario escoge la opcion 1 se realiza el area del cuadrado
+    if eleccion == '1':
+        #Se solicita la medida del lado del cuadrado
+        lado = float(input("Ingrese la longitud del lado del cuadrado: "))
+        #Se muestra el resultado
+        print("El area del cuadrado es:", area_cuadrado(lado))
+    #Si el usuario escoge la opcion 2 se realiza el area del triangulo
+    elif eleccion == '2':
+        #Se solicitan la base y la altura del triangulo
+        base = float(input("Ingrese la base del triangulo: "))
+        altura = float(input("Ingrese la altura del triangulo: "))
+        #Se muestra el resultado
+        print("El area del triangulo es:", area_triangulo(base, altura))
+    #Si el usuario escoge la opcion 3 se realiza la suma
+    elif eleccion == '3':
+        #Se solicitan los numeros a sumar
+        num1 = float(input("Ingrese el primer numero: "))
+        num2 = float(input("Ingrese el segundo numero: "))
+        #Se muestra el resultado
+        print("La suma es:", suma(num1, num2))
+    #Si el usuario escoge la opcion 4 se sale del programa
+    elif eleccion == '4':
+        print("Saliendo del programa.")
+        break
+
+
+
 
